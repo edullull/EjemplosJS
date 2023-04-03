7b77e450638f0dceb5e7ec83211f5ea423f13d49
Eduardo Llull elena <edullull@MacBook-Air-de-Eduardo.local>
2023-04-03 13:12:04 +0100

    new change

---
+++ index.js
@@ -0,0 +1,11 @@
+//function
+function MyFunction(a, b){
+    return a * b;
+}
+
+console.log(MyFunction(5,5));
+
+//para poder ejecutar un codigo JS por la terminal 
+// escribir "node index.js"
+
+console.log(MyFunction(24, 30));
