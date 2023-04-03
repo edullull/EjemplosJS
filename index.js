5982376dfb31c0919be89a10bfd28fdc47b30201
Eduardo Llull elena <edullull@MacBook-Air-de-Eduardo.local>
2023-04-03 13:14:53 +0100

    new change for notes

--- index.js
+++ index.js
@@ -8,4 +8,11 @@ console.log(MyFunction(5,5));
 //para poder ejecutar un codigo JS por la terminal 
 // escribir "node index.js"
 
+//**************************************************** */
+// gitHub
+//git add index.js
+//git commit -m "Agrego cambios al archivo index.html"
+//git push origin main
+
+
 console.log(MyFunction(24, 30));
