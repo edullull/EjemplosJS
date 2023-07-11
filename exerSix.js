1f50bcfe30527316d2eafbc62ac7828f846221a7
edullull <edullull@MacBook-Air-de-Eduardo.local>
2023-07-11 12:25:52 +0100

    add exercises 6

---
+++ exerSix.js
@@ -0,0 +1,12 @@
+//git branch -M mainhttps://github.com/edullull/EjemplosJS.git
+
+// bucle do-while siempre se ejecuta una vez
+// Ejemplo con funcion confirm 
+
+let respuesta
+
+do{
+    respuesta = confirm("¿Me gusta JS?");
+}while(respuesta)
+
+// este ejemplo hay que usarlo en el navegador del chrome(funciona)
