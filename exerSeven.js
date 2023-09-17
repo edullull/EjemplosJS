04728a158110fa3f6593a4a0d119d3cf52fda11a
edullull <edullull@MacBook-Air-de-Eduardo.local>
2023-09-17 16:55:28 +0100

    CambioDeToken

--- exerSeven.js
+++ exerSeven.js
@@ -7,4 +7,4 @@ for(let number =1; number <= 10; number++){
 for(let number = 1; number <= 10; number++){
     console.log(number);
 }
-//2ac43123a815cd27085b1e3549b04f28c6fab587
+//*******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
