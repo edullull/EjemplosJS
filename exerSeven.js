96d89fd577dd6b02abdb6f578f532cbfe751eb85
unknown <edullull99@gmail.com git config --global user.name>
2023-09-19 07:34:37 +0100

    añado otro ejemplo del FOR¡

--- exerSeven.js
+++ exerSeven.js
@@ -10,3 +10,9 @@ for(let number = 1; number <= 10; number++){
 //***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*/
 
 // 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
+
+//otro ejemplo de for
+
+for(let number=20; number >=0; number--){
+    console.log(number);
+}
